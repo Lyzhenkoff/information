cc2f9c85beba54a215497392587f848df1235448
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-12-29 18:10:41 +0300

    summary

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const index = () => {
+    return(
+        <div>Kirill Lyzhenkoff </div>
+        <div> Skills:HTML,CSS,JS,TS,LESS,WebPack,Vue,React </div>
+    )
+}
+
+ReactDOM.render(<index />, document.getElementById('root'));
+
+
